a14218111ae0810d68bc1e555b05911ac67936e1
Narvils <gustavs.gn@gmail.com>
2022-01-18 12:01:20 +0200

    2 versija

--- src/Atzimju_kalkulators.java
+++ src/Atzimju_kalkulators.java
@@ -1,9 +1,32 @@
+import javax.swing.JOptionPane;
 
 public class Atzimju_kalkulators {
-
+   
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int prieksmeti = Integer.parseInt(JOptionPane.showInputDialog("Cik macibas gribat vertet?"));
+		String[] Macibas = new String[prieksmeti];
+		int[] svars = new int[prieksmeti];
+		for(int i=0; i<=prieksmeti-1; i++){
+			Macibas[i] = JOptionPane.showInputDialog("Kads bus prieksmets?");
+		}
+		int svarigums=100;
+		int pags=0;
+		boolean pari = false;
+		for(int i=0; i<=prieksmeti-1; i++){
+		do{
+		
+		    svars[i] = Integer.parseInt(JOptionPane.showInputDialog("Kads bus svarigums "+Macibas[i]+" ir iespejami but tik svarigai "+svarigums));
+		    pags = svarigums;
+		    svarigums = svarigums - svars[i];
+		    
+		    if(svarigums<0 || svarigums>100 ){
+		    	svarigums = pags;
+		    	}
+			
+		}while(svarigums<0 || svarigums>100 || svars[i]<0 || svars[i]>100);
+		}
+		}
 
 	}
 
-}
+
