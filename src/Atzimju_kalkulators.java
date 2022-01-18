4865912529d336f1aef4c8b24b877c956d3f85cf
Narvils <gustavs.gn@gmail.com>
2022-01-18 12:16:54 +0200

    versija 3 nestrada

--- src/Atzimju_kalkulators.java
+++ src/Atzimju_kalkulators.java
@@ -6,12 +6,15 @@ public class Atzimju_kalkulators {
 		int prieksmeti = Integer.parseInt(JOptionPane.showInputDialog("Cik macibas gribat vertet?"));
 		String[] Macibas = new String[prieksmeti];
 		int[] svars = new int[prieksmeti];
+		String[] skolens = new String[prieksmeti];
+		int[][] vertejums = new int[prieksmeti][prieksmeti];
+		int[] galaV = new int[prieksmeti];
 		for(int i=0; i<=prieksmeti-1; i++){
 			Macibas[i] = JOptionPane.showInputDialog("Kads bus prieksmets?");
 		}
 		int svarigums=100;
 		int pags=0;
-		boolean pari = false;
+		
 		for(int i=0; i<=prieksmeti-1; i++){
 		do{
 		    do{
@@ -23,9 +26,15 @@ public class Atzimju_kalkulators {
 		    if(svarigums<0 || svarigums>100 ){
 		    	svarigums = pags;
 		    	}
-			
 		}while(svars[i]<0 || svars[i]>100);
 		}
+		for(int i=0; i<=prieksmeti-1; i++){
+			skolens[i] = JOptionPane.showInputDialog("Kads ir skolena vards?");
+			for(int j=1; j<=prieksmeti; j++){
+			vertejums[i][0] = Integer.parseInt(JOptionPane.showInputDialog("Kads ir vina vertejums "+Macibas[j]));
+		}
+		}
+		
 		}
 
 	}
