4e406d73196b107ab1aaf896ad0e8bb6296a6e3a
Narvils <gustavs.gn@gmail.com>
2022-02-02 13:11:37 +0200

    Sakuma mods

--- src/Atzimju_kalkulators.java
+++ src/Atzimju_kalkulators.java
@@ -1,42 +1,73 @@
-import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Atzimju_kalkulators {
-   
 	public static void main(String[] args) {
-		int prieksmeti = Integer.parseInt(JOptionPane.showInputDialog("Cik macibas gribat vertet?"));
-		String[] Macibas = new String[prieksmeti];
-		int[] svars = new int[prieksmeti];
-		String[] skolens = new String[prieksmeti];
-		int[][] vertejums = new int[prieksmeti][prieksmeti];
-		int[] galaV = new int[prieksmeti];
-		for(int i=0; i<=prieksmeti-1; i++){
-			Macibas[i] = JOptionPane.showInputDialog("Kads bus prieksmets?");
-		}
-		int svarigums=100;
-		int pags=0;
+		int studSk, kritSk;
+		Scanner scan = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("0.#");
+		     
+		do {
+			System.out.println("Cik studentiem aprēķināsi gala vērtējumu?");
+			studSk = scan.nextInt();
+		}while(studSk<1);
+		String[] studenti = new String[studSk];
 		
-		for(int i=0; i<=prieksmeti-1; i++){
-		do{
-		    do{
-		    svars[i] = Integer.parseInt(JOptionPane.showInputDialog("Kads bus svarigums "+Macibas[i]+" ir iespejami but tik svarigai "+svarigums));
-		    }while(svars[i]>svarigums);
-		    pags = svarigums;
-		    svarigums = svarigums - svars[i];
-		    
-		    if(svarigums<0 || svarigums>100 ){
-		    	svarigums = pags;
-		    	}
-		}while(svars[i]<0 || svars[i]>100);
-		}
-		for(int i=0; i<=prieksmeti-1; i++){
-			skolens[i] = JOptionPane.showInputDialog("Kads ir skolena vards?");
-			for(int j=1; j<=prieksmeti; j++){
-			vertejums[i][0] = Integer.parseInt(JOptionPane.showInputDialog("Kads ir vina vertejums "+Macibas[j]));
-		}
+		do {
+			System.out.println("Kāds būs kritēriju skaits?");
+			kritSk = scan.nextInt();
+		}while(kritSk<1);
+		String[] kriteriji = new String[kritSk];
+		int[] kriterijaSvars = new int[kritSk];
+		int[][] kriterijaVertejums = new int[studSk][kritSk];
+		double[] semestraVertejums = new double[studSk];
+		
+		//Ievada studentu vārdus, uzvārdus
+		for(int i=0; i<studenti.length; i++) {
+			System.out.println("Ievadi "+(i+1)+". studentu");
+			studenti[i] = scan.next();
 		}
 		
+		//Definē kritērijus
+		int maxSvars = 100;
+		for(int i=0; i<kriteriji.length; i++) {
+			System.out.println("Ievadi "+(i+1)+". kritēriju");
+			kriteriji[i] = scan.next();
+			//Norāda katra kritērija svaru
+			do {
+				System.out.println("Ievadi "+(i+1)+". kritērija svaru");
+				kriterijaSvars[i] = scan.nextInt();
+			}while(kriterijaSvars[i]>maxSvars || 
+					kriterijaSvars[i]<1 || 
+					(kriterijaSvars[0]==100 && kritSk > 1));
+			maxSvars -= kriterijaSvars[i];
 		}
-
+		
+		//Norāda vērtējumu kādu ieguvis katrs students par katru kritēriju
+		for(int i=0; i<kriterijaVertejums.length; i++) {
+			for(int j=0; j<kriterijaVertejums[i].length; j++) {
+				do {
+					System.out.println("Ievadi "+studenti[i]+" vērtējumu par kritēriju "+kriteriji[j]);
+					kriterijaVertejums[i][j] = scan.nextInt();
+				}while(kriterijaVertejums[i][j]<0 || kriterijaVertejums[i][j]>10);
+			}
+		}
+		
+		double rezultats;
+		for(int i=0; i<studenti.length; i++) {
+			rezultats=0;
+			for(int j=0; j<kriteriji.length; j++) {
+				rezultats += ((double) kriterijaSvars[j]/100)*kriterijaVertejums[i][j];
+			}
+			semestraVertejums[i] = rezultats;
+		}
+		
+		for(int i=0; i<studenti.length; i++) {	
+			for(int j=0; j<kriteriji.length; j++) {
+				System.out.println("Studenta "+studenti[i]+" vērtējums par kritēriju "+kriteriji[j]+" ir "+kriterijaVertejums[i][j]+", kura svars ir "+kriterijaSvars[j]);
+			}
+			System.out.println("Semestra vērtējums ir "+df.format(semestraVertejums[i])+"\n");
+		}
+		scan.close();
 	}
-
-
+}
